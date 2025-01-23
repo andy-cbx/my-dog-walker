ab3afa61f719008f63cf416aa1b64edddf3257fa
xbrownx <asbrownn@gmail.com>
2025-01-23 19:05:30 +1100

    first push

--- src/routes/blog/blog-route.tsx
+++ src/routes/blog/blog-route.tsx
@@ -1,1 +1,1 @@
-import oops from '@/assets/oops.svg';const BlogRoute = () => {    return (        <section            className={                'flex flex-col items-center justify-center bg-primary-blue pt-32'            }        >            <img src={oops} alt={''} />        </section>    );};export default BlogRoute;
+import oops from '@/assets/oops.svg';const BlogRoute = () => {	return (		<section			className={				'flex flex-col items-center justify-center bg-primary-blue pt-32'			}		>			<img src={oops} alt={''} />		</section>	);};export default BlogRoute;
